d3c5894f4d7a81fa0fccd7a9d08a02277f470fca
Marcin Jahn <jahnmarcin@outlook.com>
2018-06-26 17:38:46 +0200

    Added existing project files

---
+++ Controllers/TokenController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+using MJIoT_EFCoreModel;
+
+namespace MJIoT_TokenIssuer.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Token")]
+    public class TokenController : Controller
+    {
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult RequestToken([FromBody] TokenRequest request)
+        {
+            var userId = GetUserId(request);
+
+            if (userId.HasValue)
+            {
+                var claims = new[]
+                {
+                    //new Claim(ClaimTypes.Name, request.Username),
+                    new Claim("iss", "MJIoT Authentication Service"),
+                    new Claim("sub", userId.Value.ToString()),
+                    new Claim("exp", DateTimeOffset.UtcNow.AddMonths(6).ToUnixTimeSeconds().ToString()),
+                };
+
+                //var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(GetPrivateKey()));
+                //var creds = new SigningCredentials(key, SecurityAlgorithms.RsaSha256);
+
+                //var token = new JwtSecurityToken(
+                //    issuer: "yourdomain.com",
+                //    audience: "yourdomain.com",
+                //    claims: claims,
+                //    expires: DateTime.Now.AddMinutes(30),
+                //    signingCredentials: creds);
+
+                var manager = new TokenManager();
+                var token = manager.CreateToken(claims);
+
+                return Ok(token);
+            }
+
+            return Unauthorized();
+        }
+
+
+
+        private int? GetUserId(TokenRequest request)
+        {
+            MJIoT_EFCoreModel.Models.Users userCheck;
+            using (var context = new MJIoTDbContext())
+            {
+                userCheck = context.Users
+                    .Where(n => n.Login == request.Username && n.Password == request.Password)
+                    .FirstOrDefault();
+            }
+
+            if (userCheck != null)
+                return userCheck.Id;
+            else
+                return null;
+        }
+
+
+        //// GET: api/Token
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        //// GET: api/Token/5
+        //[HttpGet("{id}", Name = "Get")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+        
+        //// POST: api/Token
+        ////[HttpPost]
+        ////public void Post([FromBody]string value)
+        ////{
+        ////}
+        
+        //// PUT: api/Token/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody]string value)
+        //{
+        //}
+        
+        //// DELETE: api/ApiWithActions/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ TokenManager.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.IO;
+//using System.Security.Cryptography;
+
+//using Org.BouncyCastle.Crypto;
+//using Org.BouncyCastle.Crypto.Parameters;
+//using Org.BouncyCastle.OpenSsl;
+using System.Security.Cryptography.X509Certificates;
+using Microsoft.Extensions.Configuration;
+
+namespace MJIoT_TokenIssuer
+{
+    public class TokenManager
+    {
+
+        private readonly ICertificateLoader _certificateLoader;
+
+        public TokenManager()
+        {
+            //_certificateLoader = new LocalCertificateLoader();
+            _certificateLoader = new LocalCertificateLoader();
+        }
+
+        private void HS256Test()
+        {
+            var payload = new Dictionary<string, object>()
+            {
+                { "sub", "mr.x@contoso.com" },
+                { "exp", 1300819380 }
+            };
+
+            var secretKey = new byte[] { 115, 101, 99, 114, 101, 116, 49, 50, 51 };
+
+            string token = Jose.JWT.Encode(payload, secretKey, Jose.JwsAlgorithm.HS256);
+            //string re = "";
+        }
+
+        //signed
+        private void RS256Test()
+        {
+            var payload = new Dictionary<string, object>()
+            {
+                { "sub", "mr.x@contoso.com" },
+                { "exp", 1300819380 }
+            };
+
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "Certificates", "mycert.pfx");
+            var cert = new X509Certificate2(filePath, "qwerty1");
+
+            var privateKey = cert.GetRSAPrivateKey();
+
+            string token = Jose.JWT.Encode(payload, privateKey, Jose.JwsAlgorithm.RS256);//.Replace("TJuxjsI2", "TJuxisI2");
+
+            //walidacja
+            var publicKey = cert.GetRSAPublicKey();
+            string json = Jose.JWT.Decode(token, publicKey);
+
+            //string re = "";
+        }
+
+
+
+        public string CreateToken(IEnumerable<Claim> claims)
+        {
+            Dictionary<string, object> payload = claims.ToDictionary(k => k.Type, v => (object)v.Value);
+
+            var cert = _certificateLoader.LoadCertificate();
+            var privateKey = cert.GetRSAPrivateKey();
+
+            string token = Jose.JWT.Encode(payload, privateKey, Jose.JwsAlgorithm.RS256);
+
+            return token;
+        } 
+
+
+        //public string CreateToken(IEnumerable<Claim> claims, string certificateName, string rootFolder, string certificateFolder = "Certificates")
+        //{
+        //    RS256Test();
+
+        //    string path = Path.Combine(rootFolder, certificateFolder, certificateName);
+        //    string pemString = File.ReadAllText(path);
+        //    string jwt = string.Empty;
+        //    AsymmetricCipherKeyPair keyPair;
+
+        //    using (StreamReader sr = new StreamReader(path))
+        //    {
+        //        PemReader pr = new PemReader(sr);
+        //        keyPair = (AsymmetricCipherKeyPair)pr.ReadObject();
+        //    }
+
+        //    RSAParameters rsaParams = ToRSAParameters((RsaPrivateCrtKeyParameters)keyPair.Private);
+
+        //    using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+        //    {
+        //        rsa.ImportParameters(rsaParams);
+        //        Dictionary<string, object> payload = claims.ToDictionary(k => k.Type, v => (object)v.Value);
+        //        jwt = Jose.JWT.Encode(payload, rsa, Jose.JwsAlgorithm.RS256);
+        //    }
+
+        //    return jwt;
+        //}
+
+        //public RSAParameters ToRSAParameters(RsaKeyParameters rsaKey)
+        //{
+        //    RSAParameters rp = new RSAParameters { Modulus = rsaKey.Modulus.ToByteArrayUnsigned() };
+        //    if (rsaKey.IsPrivate)
+        //        rp.D = rsaKey.Exponent.ToByteArrayUnsigned();
+        //    else
+        //        rp.Exponent = rsaKey.Exponent.ToByteArrayUnsigned();
+        //    return rp;
+        //}
+
+        //public RSAParameters ToRSAParameters(RsaPrivateCrtKeyParameters privKey)
+        //{
+        //    RSAParameters rp = new RSAParameters
+        //    {
+        //        Modulus = privKey.Modulus.ToByteArrayUnsigned(),
+        //        Exponent = privKey.PublicExponent.ToByteArrayUnsigned(),
+        //        D = privKey.Exponent.ToByteArrayUnsigned(),
+        //        P = privKey.P.ToByteArrayUnsigned(),
+        //        Q = privKey.Q.ToByteArrayUnsigned(),
+        //        DP = privKey.DP.ToByteArrayUnsigned(),
+        //        DQ = privKey.DQ.ToByteArrayUnsigned(),
+        //        InverseQ = privKey.QInv.ToByteArrayUnsigned()
+        //    };
+        //    return rp;
+        //}
+    }
+}
